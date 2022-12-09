faab10cbc7f4da7541c39ef6bdfcd96eb2519e9c
SlavBrain <82366112+SlavBrain@users.noreply.github.com>
2022-12-09 15:42:23 +0300

    Bug with Division segment

--- Assets/Scripts/Rope/Rope.cs
+++ Assets/Scripts/Rope/Rope.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Rope : MonoBehaviour
 {
-    [SerializeField]private List<RopeSegment> _segments;
-    [SerializeField]private GameObject _ropeTemplate;
-    [SerializeField] private float _thickness=0.1f;
+    [SerializeField] private List<RopeSegment> _segments;
+    [SerializeField] private int _minSegmentsCount = 50;
+    [SerializeField] private GameObject _ropeTemplate;
+    [SerializeField] private float _thickness = 0.1f;
     [SerializeField] private LineRenderer _lineRenderer;
     [SerializeField] private Vector3[] _dots;
-    
+
     private void OnEnable()
     {
         _dots = new Vector3[_lineRenderer.positionCount];
         _lineRenderer.GetPositions(_dots);
-           Init(_dots);
+        Init(_dots);
     }
 
     public void Init(Vector3[] dots)
@@ -24,12 +25,41 @@ public class Rope : MonoBehaviour
 
         for (int i = 1; i < dots.Length; i++)
         {
-                RopeSegment newSegment = Instantiate(_ropeTemplate, transform).AddComponent<RopeSegment>();
-                _segments.Add(newSegment); 
-                newSegment.Init(dots[i], _segments[i-1], _thickness);
+            RopeSegment newSegment = Instantiate(_ropeTemplate, transform).AddComponent<RopeSegment>();            
+            _segments.Add(newSegment);
+            newSegment.Init(dots[i], _segments[i - 1], _thickness,this);
         }
 
-        
-        firstNewSegment.Init(dots[0], _segments[_segments.Count - 1], _thickness);
+
+        firstNewSegment.Init(dots[0], _segments[_segments.Count - 1], _thickness,this);
+    }
+
+    public void SegmentDivision(RopeSegment oldSegment)
+    {
+        Vector3 breakPoint = (oldSegment.StartPoint + oldSegment.PreviousSegment.StartPoint) / 2;
+        RopeSegment newSegment = AddNewSegment();
+        newSegment.Init(breakPoint, oldSegment.PreviousSegment, _thickness,this);
+        oldSegment.Init(oldSegment.StartPoint, newSegment, _thickness,this);
+
+        Debug.Log("division");
+    }
+
+    public void DeleteSegment(RopeSegment segment)
+    {
+        if (_segments.Count > _minSegmentsCount)
+        {
+            Debug.Log("delete");
+            segment.NextSegment.ChangePreviousSegment(segment.PreviousSegment);
+            //segment.PreviousSegment.ChangeNextSegment(segment.NextSegment);
+            Destroy(segment.gameObject);
+            //segment.gameObject.SetActive(false);
+        }        
+    }
+
+    private RopeSegment AddNewSegment()
+    {
+        RopeSegment newSegment = Instantiate(_ropeTemplate, transform).AddComponent<RopeSegment>();
+        _segments.Add(newSegment);
+        return newSegment;
     }
 }
--- Assets/Scripts/Rope/RopeSegment.cs
+++ Assets/Scripts/Rope/RopeSegment.cs
@@ -6,39 +6,69 @@ using UnityEngine;
 public class RopeSegment : MonoBehaviour
 {
     [SerializeField] private const float _minLenght = 0.1f;
+    [SerializeField] private Vector3 _endPoint;
     [SerializeField] private Vector3 _startPoint;
     [SerializeField] private RopeSegment _nextSegment;
+    [SerializeField] private RopeSegment _previousSegment;
     [SerializeField] private float _lenght;
     [SerializeField] private float _thickness;
-    [SerializeField] private Collider _collider;
-    [SerializeField] private bool _isBarrierTouch;
+    [SerializeField] private bool _isBarrierTouch = false;
+    [SerializeField] private bool _startPointLock = false;
+    [SerializeField] private bool _endPointLock = false;
 
-    public Vector3 StartPoint => _startPoint;
-    private void Awake()
-    {
-        _collider = GetComponentInChildren<Collider>();
-    }
+    public event Action<RopeSegment> BarrierTouched;
+    public event Action<RopeSegment> PreviousSegmentChanged;
+    public event Action<RopeSegment> MinimalLenghtReached;
+
+    public Vector3 StartPoint => _previousSegment.EndPoint;
+    public Vector3 EndPoint => _endPoint;
+    public RopeSegment PreviousSegment => _previousSegment;
+
+    public RopeSegment NextSegment => _nextSegment;
+
+    public bool IsBarrierTouch => _isBarrierTouch;
+
+
     private void Update()
     {
-        Decrease();
-        UpdateState();
+        CheckNeiboursSegments();
+        if (!_isBarrierTouch)
+        {
+            Decrease();
+            UpdateState();
+
+        }
+
+    }
+
+    private void CheckNeiboursSegments()
+    {
+        _startPointLock = _previousSegment == null ? true : _previousSegment.IsBarrierTouch;
+        _endPointLock = _nextSegment == null ? true : _nextSegment.IsBarrierTouch;
+
     }
 
     private void Decrease()
     {
-        if (!_isBarrierTouch)
+        if (_lenght > _minLenght)
         {
-            _startPoint = Vector3.MoveTowards(_startPoint, _nextSegment.StartPoint, Time.deltaTime * 10f);
+            if (!_endPointLock)
+                _endPoint = Vector3.MoveTowards(_endPoint, _startPoint, Time.deltaTime * 10f);
+            else if (!_startPointLock)
+                _startPoint = Vector3.MoveTowards(_startPoint, _endPoint, Time.deltaTime * 10f);
         }
     }
 
-    public void Init(Vector3 startPoint, RopeSegment nextSegment, float thickness)
+    public void Init(Vector3 endPoint, RopeSegment previousSegment, float thickness, Rope rope)
     {
-        _startPoint = startPoint;
-        _nextSegment = nextSegment;
+        _endPoint = endPoint;
+        ChangePreviousSegment(previousSegment);
         _thickness = thickness;
+        //_previousSegment.ChangeNextSegment(this);
 
-        UpdateState();
+        BarrierTouched += rope.SegmentDivision;
+        MinimalLenghtReached += rope.DeleteSegment;
+        //UpdateState();
     }
 
     public void UpdateState()
@@ -50,12 +80,19 @@ public class RopeSegment : MonoBehaviour
 
     private void RefreshPosition()
     {
+        _startPoint = _previousSegment.EndPoint;
+
+        if (_endPointLock)
+        {
+            _endPoint = _nextSegment.StartPoint;
+        }
+
         transform.position = _startPoint;
     }
 
     private void RefreshLenght()
     {
-        float lenght = Vector3.Distance(_startPoint, _nextSegment.StartPoint);
+        float lenght = Vector3.Distance(_endPoint, _startPoint);
 
         if (lenght > _minLenght)
         {
@@ -64,6 +101,11 @@ public class RopeSegment : MonoBehaviour
         else
         {
             _lenght = _minLenght;
+
+            if (!_isBarrierTouch && !_startPointLock && !_endPointLock)
+            {
+                MinimalLenghtReached?.Invoke(this);
+            }
         }
 
         transform.localScale = new Vector3(_thickness, _lenght, _thickness);
@@ -71,14 +113,43 @@ public class RopeSegment : MonoBehaviour
 
     private void RefreshRotation()
     {
-        transform.LookAt(_nextSegment.StartPoint, Vector3.up);
+        if (_nextSegment != null)
+        {
+            transform.LookAt(_nextSegment.StartPoint, Vector3.up);
+        }
+
         transform.Rotate(90, 0, 0);
     }
 
-    private void OnCollisionStay(Collision collision)
+    public void ChangePreviousSegment(RopeSegment previousSegment)
+    {
+        //if (_previousSegment != previousSegment&&_previousSegment!=null)
+        //{
+        //    PreviousSegmentChanged?.Invoke(previousSegment);
+        //_previousSegment.ChangeNextSegment(previousSegment);            
+        //}
+
+        _previousSegment = previousSegment;
+        _previousSegment.ChangeNextSegment(this);
+        //PreviousSegmentChanged = previousSegment.ChangeNextSegment;
+    }
+
+    public void ChangeNextSegment(RopeSegment nextSegment)
+    {
+        _nextSegment = nextSegment;
+    }
+
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.TryGetComponent<Barrier>(out Barrier barrier))
         {
+            Debug.Log("touched");
+
+            if (_lenght > _minLenght)
+            {
+                BarrierTouched?.Invoke(this);
+            }
+            
             _isBarrierTouch = true;
         }
     }
