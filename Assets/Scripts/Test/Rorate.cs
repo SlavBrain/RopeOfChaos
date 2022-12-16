2f02abb895db2692d6abb11bd1629bd97362b11e
Egor Borisov <82366112+SlavBrain@users.noreply.github.com>
2022-12-16 20:18:54 +0300

    failed Test

--- Assets/Scripts/Test/Rorate.cs
+++ Assets/Scripts/Test/Rorate.cs
@@ -1,16 +1,13 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Transforming : MonoBehaviour
+public class Rorate : MonoBehaviour
 {
     [SerializeField] Transform _target;
-    [SerializeField] float _speed;
+    [SerializeField] float speed;
     [SerializeField] float _angle;
     [SerializeField] float _sign;
-    [SerializeField] HingeJoint _springJoint;
-    float MINsIZE = 0.4f;
 
     private void Update()
     {
@@ -19,24 +16,15 @@ public class Transforming : MonoBehaviour
         //_angle = _angle * _sign;
         if (Input.GetMouseButtonDown(0))
         {
-            //Rotate();
+            Rotate();
         }
         //
     }
 
     public void Rotate()
     {
-        //transform.eulerAngles = new Vector3(transform.rotation.x,  _angle, transform.rotation.z);
+        transform.eulerAngles = new Vector3(transform.rotation.x,  _angle, transform.rotation.z);
         //transform.rotation = Quaternion.EulerAngles(90, 0, transform.rotation.z);
         //transform.rotation = new Quaternion(45, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-    }
-
-    public void Decrease()
-    {        
-        transform.localScale = new Vector3(transform.localScale.x, Mathf.MoveTowards(transform.localScale.y,MINsIZE,_speed), transform.localScale.z);
-        if (_springJoint.connectedBody != null)
-        {
-            _springJoint.connectedAnchor = _target.transform.position-transform.position;
-        }
     }
 }
